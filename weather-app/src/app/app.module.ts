1a91a62a9f0ffb6d844d9935a9b0967cf8cc2148
Juan Pedro Ortega Alguacil <68635318+JuanPedroOrtega@users.noreply.github.com>
2023-02-10 17:16:27 +0100

    Creacion del Proyecto, y carpeta Components, Pages y componente Home-Page

---
+++ weather-app/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {HomePageComponent} from "./components/pages/home-page/home-page.component";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
