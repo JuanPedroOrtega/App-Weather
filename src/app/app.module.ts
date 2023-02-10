52d5d9d2e5e5f4a672117948532edb8b6b0a25dd
Eduardo ASTOLFI <eduardo.astolfi@prima-solutions.com>
2023-02-10 18:09:16 +0100

    Change indentation to 4 spaces

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {HomePageComponent} from "./components/pages/home-page/home-page.component";
+import { HomePageComponent } from './components/pages/home-page/home-page.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomePageComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, HomePageComponent],
+    imports: [BrowserModule, AppRoutingModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
