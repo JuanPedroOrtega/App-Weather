518f1c51b09e9a50ee1dda45778a3ac87687e6e6
Eduardo ASTOLFI <eduardo.astolfi@prima-solutions.com>
2023-03-01 09:27:44 +0100

    Add some weather icons

--- src/app/components/shared/ui/ui.module.ts
+++ src/app/components/shared/ui/ui.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 
 import { CoreModule } from 'src/app/modules/core/core.module';
+
 import { NavigationComponent } from './navigation/navigation.component';
+import { WeatherIconComponent } from './weather-icon/weather-icon.component';
 
 const declarations = [
     NavigationComponent,
+    WeatherIconComponent,
 ]
 
 @NgModule({
